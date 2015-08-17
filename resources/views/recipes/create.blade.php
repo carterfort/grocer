cf94ff743799930a8eacaace6be456dee2600d83
Carter Fort <carter.fort@gmail.com>
2015-08-17 09:47:51 -0400

    Added the "old" values when the create form fails validation

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -15,22 +15,22 @@
 
         <div class="row">
             <div class="col-sm-3">
-               <input type="text" name="name" placeholder="Recipe Name" class="form-control" />
+               <input type="text" name="name" placeholder="Recipe Name" class="form-control" value="{{ old("name") }}"/>
             </div>
             <div class="col-sm-4">
-                <input type="text" name="url" placeholder="Reference URL" class="form-control"/>
+                <input type="text" name="url" placeholder="Reference URL" class="form-control" value="{{ old("url") }}"/>
             </div>
             <div class="col-sm-5">
-                <input type="text" name="photo_url" placeholder="Photo URL" class="form-control "/>
+                <input type="text" name="photo_url" placeholder="Photo URL" class="form-control " value="{{ old("photo_url") }}"/>
             </div>
         </div>
         <br/>
         <div class="row">
             <div class="col-sm-6">
-                <textarea name="ingredients" rows="5" placeholder="Ingredients" class="form-control "></textarea>
+                <textarea name="ingredients" rows="5" placeholder="Ingredients" class="form-control ">{{ old('ingredients') }}</textarea>
             </div>
             <div class="col-sm-6">
-                <textarea name="instructions" rows="8" placeholder="Step-by-step..." class="form-control"></textarea>
+                <textarea name="instructions" rows="8" placeholder="Step-by-step..." class="form-control">{{ old('instructions') }}</textarea>
             </div>
         </div>
         <br/>
